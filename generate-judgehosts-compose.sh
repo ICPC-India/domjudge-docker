86e150f64a505021e8e31d242cc904a4066c6a9d
anup <anup.kalbalia@gmail.com>
2024-10-17 21:33:07 +0200

    added gitignore, implemeted compose genration for multiple judgehosts through template, removed irrelevant files

---
+++ generate-judgehosts-compose.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Number of judgehosts to deploy (passed as the first argument)
+NUM_JUDGEHOSTS=$1
+
+if [ -z "$NUM_JUDGEHOSTS" ]; then
+  echo "Please provide the number of judgehost instances to deploy."
+  echo "Usage: ./generate-compose.sh <num_instances>"
+  exit 1
+fi
+
+# Start with an empty docker-compose.yml file
+> judgehost-dc.yml
+
+# Copy the static parts of the docker-compose.yml file (if any)
+cat <<EOF >> judgehost-dc.yml
+services:
+EOF
+
+# Loop through and create judgehost instances
+for ((i=1; i<=NUM_JUDGEHOSTS; i++))
+do
+  # Replace the placeholder {{ID}} with the actual number
+  sed "s/{{ID}}/$i/g" judgehost-dc.template >> judgehost-dc.yml
+done
+
+# Now you can use docker-compose up to launch all instances
+docker compose -f judgehost-dc.yml up -d
--- start-judgehosts.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-num_judgehosts=${1:-1}  # Default to 1 if no argument is provided
-
-for i in $(seq 1 $num_judgehosts)
-do
-  DAEMON_ID=$i docker-compose -f judgehost.yml up -d --no-recreate --scale judgehost=1
-done
