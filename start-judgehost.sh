b9d294ae0289932fb3395e1ab0fae70b3548eac5
anup <anup.kalbalia@gmail.com>
2024-10-23 16:36:38 +0200

    replaced octet with ip-address

--- start-judgehost.sh
+++ start-judgehost.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
+IP_ADDRESS=$(echo "$IP_ADDRESS" | sed 's/\./-/g')
 export IP_ADDRESS
 export JUDGEHOST_PASSWORD=GIAchaw2fUOkIkTI+AHbMh9NVmXbiMJc
 export DOMSERVER_MAIN_IP='10.0.2.95'
