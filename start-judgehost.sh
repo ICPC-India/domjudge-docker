c41fc5a8823819424e35d8196b53f5ebdb2da0e5
anup <anup.kalbalia@gmail.com>
2024-10-23 13:02:46 +0200

    added code for removing existing judgehost containers

--- start-judgehost.sh
+++ start-judgehost.sh
@@ -4,5 +4,5 @@ DAEMON_ID=$(echo $IP_ADDRESS | awk -F. '{print $4}')
 export DAEMON_ID
 export JUDGEHOST_PASSWORD=GIAchaw2fUOkIkTI+AHbMh9NVmXbiMJc
 export DOMSERVER_MAIN_IP='10.0.2.95'
-docker compose -f judgehost.yml down
+docker container rm -f $(docker container ls -q)
 docker compose -f judgehost.yml up -d
