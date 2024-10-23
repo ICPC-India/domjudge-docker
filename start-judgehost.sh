b80d14b90a96a36b70f2b73f29bd6cc925222349
anup <anup.kalbalia@gmail.com>
2024-10-23 12:40:30 +0200

    added env variables to start-judgehost

--- start-judgehost.sh
+++ start-judgehost.sh
@@ -2,4 +2,7 @@
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
 DAEMON_ID=$(echo $IP_ADDRESS | awk -F. '{print $4}')
 export DAEMON_ID
+export JUDGEHOST_PASSWORD=GIAchaw2fUOkIkTI+AHbMh9NVmXbiMJc
+export DOMSERVER_MAIN_IP='10.0.2.95'
+docker compose -f judgehost.yml down
 docker compose -f judgehost.yml up -d
