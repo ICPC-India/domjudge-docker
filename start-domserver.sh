ddde0b83374dd26f22d1be39e33d727bb985d22d
amitsingh3396 <f2014214p@alumni.bits-pilani.ac.in>
2024-10-28 22:50:11 +0530

    Modified the domserver yml files for redis and with redis feature and modified the start-domserver file to run the suitable domserver.

--- start-domserver.sh
+++ start-domserver.sh
@@ -5,5 +5,16 @@ export DB_ROOT_PASSWORD=QUSa5AUOjsXFYbRN2FVL
 export DJ_DB=domjudge
 export DJ_DB_USER=domjudge
 export DJ_DB_PASSWORD=djpw
+
+# Remove all running containers
 docker container rm -f $(docker container ls -q)
-docker compose -f domserver.yml up -d
+
+# Check for command line argument
+if [ "$1" == "redis" ]; then
+    # Start Redis and DOMserver with Redis configuration
+    docker compose -f domserver-redis.yml up -d
+    docker compose -f redis.yml up -d
+else
+    # Start regular DOMserver setup
+    docker compose -f domserver.yml up -d
+fi
